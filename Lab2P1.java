cf75db5999ee890394130f35f2f516904bccdb1e
Qianyi Li <qli003@student.sdccd.edu>
2025-02-06 11:59:41 -0800

    Initial sharing of project

---
+++ Lab2P1.java
@@ -0,0 +1,23 @@
+
+/**
+ * Program to show imprecision with large numbers
+ *
+ * @auther   Qianyi Li
+ * @version  1.0
+ * @since   2/6/2025
+ */
+public class Lab2P1
+{
+    public static void main(String [] args){
+        double x=12345.6789e200;
+        double y,z,zz;
+        y=1/x;
+        z=x*y;
+        zz=1-z;
+        
+        System.out.println("the value of x is:"+x);
+        System.out.println("the value of y is:"+y);
+        System.out.println("the value of z(x*y) is:"+z);
+        System.out.println("the value of 1 - z is:"+zz);
+    }
+}
---
+++ ProgChall3.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class ProgChall3 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ProgChall3
+{
+    public static void main(String[] args){
+        System.out.print("Qianyi Li"+'\n'+"Computer Sience"+'\n'+"Video Gammes");
+    }
+}
